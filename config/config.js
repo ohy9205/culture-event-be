2a76a99bd7814bb720a648e2f7420d8ed9db2adc
minki <93minki@minkiui-MacBookAir.local>
2023-11-01 10:49:28 +0900

    fix: DB 설정 수정

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: process.env.SEQUELIZE_PASSWORD,
-    database: "nodejs",
-    host: "127.0.0.1",
+    username: "seoul-event-info-user",
+    password: process.env.DATABASE_PASSWORD,
+    database: "seoul-event-info-database",
+    host: "srv-captain--jdqurlwkvo-mysql-80x",
     dialect: "mysql",
   },
 };
