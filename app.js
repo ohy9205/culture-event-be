c9f437f95d67add0c72e970021b767edf911b581
minki <93minki@minkiui-MacBookAir.local>
2023-11-01 16:10:54 +0900

    fix: 서버 host, port 수정

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.use((err, req, res, next) => {
   res.send(err);
 });
 
-app.listen(app.get("port"), () => {
+app.listen(app.get("port"), "0.0.0.0", () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
 });
