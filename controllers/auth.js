54f9e44d96d659eac39b1ad63b684cd80067287d
Minki Kim <alsrl0023@naver.com>
2023-11-07 22:02:22 +0900

    fix: sameSite None 추가

--- controllers/auth.js
+++ controllers/auth.js
@@ -75,6 +75,7 @@ exports.signIn = async (req, res, next) => {
             // path: "/",
             httpOnly: true,
             secure: true,
+            sameSite: "None",
           })
           .json({
             code: 200,
