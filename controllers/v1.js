b7145ae6614b3fb93614a647f546402e7084f5e9
minki <alsrl0023@naver.com>
2023-11-13 12:28:52 +0900

    feat: 로그인이 필요한 라우터, 필요 없는 라우터 분리

--- controllers/v1.js
+++ controllers/v1.js
@@ -6,147 +6,16 @@ const currentDate = () => {
   return new Date().toISOString().slice(0, 10);
 };
 
-exports.getEvents = async (req, res) => {
-  if (req.query.pageIndex && req.query.pageSize) {
-    // 페이지네이션 사용
-    const pageIndex = Number(req.query.pageIndex);
-    const pageSize = Number(req.query.pageSize);
-    let offset = 0;
-
-    if (pageIndex > 1) {
-      offset = pageSize * (pageIndex - 1);
-    }
-    const { category, location, isfree, keyword, start, end, latest } =
-      req.query;
-
-    let where = {};
-    let orderOption = [];
-
-    if (req.query.orderBy === "views") {
-      orderOption = [["views", "DESC"]];
-    } else if (req.query.orderBy === "latest") {
-      console.log("hello");
-      where.startDate = { [Op.gte]: currentDate() };
-      orderOption = [["startDate", "ASC"]];
-    } else {
-      orderOption = [["startDate", "ASC"]];
-    }
-    if (category) where.category = category;
-    if (location) where.location = location;
-    if (isfree) where.isFree = isfree === "무료";
-    if (keyword) where.title = { [Op.like]: `%${keyword}%` };
-    if (start && end) {
-      where.startDate = { [Op.gte]: start, [Op.lte]: end };
-    }
-
-    await Event.findAndCountAll({
-      where,
-      order: orderOption,
-      limit: pageSize,
-      offset: offset,
-    })
-      .then((events) => {
-        res.json({
-          code: 200,
-          totalPage: Math.ceil(events.count / pageSize),
-          payload: events,
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        return res.status(500).json({
-          code: 500,
-          message: "서버 에러?",
-        });
-      });
-  } else {
-    // 페이지네이션 사용 X
-    const { category, location, isfree, keyword, start, end, latest } =
-      req.query;
-
-    let where = {};
-    let orderOption = [];
-
-    if (req.query.orderBy === "views") {
-      orderOption = [["views", "DESC"]];
-    } else if (req.query.orderBy === "latest") {
-      console.log("hello");
-      where.startDate = { [Op.gte]: currentDate() };
-      orderOption = [["startDate", "ASC"]];
-    } else {
-      orderOption = [["startDate", "ASC"]];
-    }
-    if (category) where.category = category;
-    if (location) where.location = location;
-    if (isfree) where.isFree = isfree === "무료";
-    if (keyword) where.title = { [Op.like]: `%${keyword}%` };
-    if (start && end) {
-      where.startDate = { [Op.gte]: start, [Op.lte]: end };
-    }
-
-    await Event.findAll({
-      order: orderOption,
-      where,
-    })
-      .then((events) => {
-        res.json({
-          code: 200,
-          payload: events,
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        return res.status(500).json({
-          code: 500,
-          message: "서버 에러?",
-        });
-      });
-  }
-};
-
-exports.getEventsById = async (req, res, next) => {
-  // TODO 조회수 증가하는 로직 필요
-  try {
-    const eventId = req.params.id;
-    const event = await Event.findByPk(eventId, {
-      include: {
-        model: Comment,
+exports.getLatestEvent = async (req, res) => {
+  await Event.findAndCountAll({
+    where: {
+      startDate: {
+        [Op.gte]: currentDate(),
       },
-    });
-
-    if (!event) {
-      return res.status(404).json({
-        code: 404,
-        message: "해당 id의 이벤트가 없습니다.",
-      });
-    }
-    res.locals.event = event;
-    next();
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-};
-
-exports.increaseViewCount = async (req, res, next) => {
-  try {
-    const event = res.locals.event;
-
-    event.increment("views", { by: 1 });
-
-    res.json({
-      code: 200,
-      payload: event,
-    });
-  } catch (error) {
-    console.error(error);
-    next(err);
-  }
-};
-
-exports.getEventsByViews = async (req, res) => {
-  await Event.findAll({
-    order: ["views", "DESC"],
+    },
+    order: [["startDate", "ASC"]],
+    limit: 10,
+    offset: 0,
   })
     .then((events) => {
       res.json({
@@ -161,4 +30,45 @@ exports.getEventsByViews = async (req, res) => {
         message: "서버 에러?",
       });
     });
+};
+
+exports.getMostPopularEvent = async (req, res) => {
+  await Event.findAndCountAll({
+    order: [["views", "DESC"]],
+    limit: 7,
+    offset: 0,
+  })
+    .then((events) => {
+      res.json({
+        code: 200,
+        payload: events,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({
+        code: 500,
+        message: "서버 에러?",
+      });
+    });
+};
+
+exports.getEventById = async (req, res) => {
+  try {
+    const eventId = req.params.id;
+    const event = await Event.findByPk(eventId, {
+      include: {
+        model: Comment,
+      },
+    });
+
+    if (!event) {
+      return res.status(404).json({
+        code: 404,
+        message: "해당 id의 이벤트가 없습니다",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
---
+++ routes/v2.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  getEvents,
+  getEventsById,
+  increaseViewCount,
+} = require("../controllers/v1");
+
+const { verifyAccessToken } = require("../middlewares");
+
+const router = express.Router();
+
+router.get("/events", verifyAccessToken, getEvents);
+
+router.get("/events/:id", verifyAccessToken, getEventsById, increaseViewCount);
+
+module.exports = router;
