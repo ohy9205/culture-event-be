a2ea4a395348bd97e6e11865d07a676224e58caa
minki <alsrl0023@naver.com>
2023-11-15 15:40:00 +0900

    feat: 댓글 CRUD 기능 추가

--- controllers/comment.js
+++ controllers/comment.js
@@ -9,13 +9,8 @@ const User = require("../models/user");
 // 게시글 정보를 요청했을 때, 모든 유저가 작성한 댓글들이 보여야 한다.
 
 exports.addComment = async (req, res) => {
-  // body에 댓글 내용
-  // 이벤트 id도 있어야 함
-  // 로그인한 유저 정보 들어있음.
   const { user } = res.locals.user; // 로그인한 사용자 정보
   const { content, eventId } = req.body; // 댓글 내용, 이벤트 id
-  // DB에 저장
-  // 로그인한 사용자 테이블에도 추가되어야 하고, 이벤트 테이블에도 추가되어야 함
 
   try {
     const userExist = await User.findOne({ where: { email: user.email } });
@@ -33,5 +28,72 @@ exports.addComment = async (req, res) => {
     return res.json({ code: 200, message: "댓글 추가 성공" });
   } catch (err) {
     console.error(err);
+  }
+};
+
+exports.updateComment = async (req, res, next) => {
+  const commentId = req.params.id;
+  const { content } = req.body;
+  const { user } = res.locals.user;
+
+  console.log("User ID", user.id);
+
+  try {
+    const comment = await Comment.findByPk(commentId);
+    if (!comment) {
+      return res.json({
+        code: 404,
+        message: "댓글을 찾을 수 없습니다",
+      });
+    }
+
+    if (user.id !== comment.commenter) {
+      return res.status(404).json({
+        code: 404,
+        message: "본인이 아닌 사용자가 댓글 수정을 시도합니다.",
+      });
+    }
+
+    await comment.update({ content });
+
+    return res.status(200).json({
+      code: 200,
+      message: "댓글이 정상적으로 수정되었습니다.",
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+exports.deleteComment = async (req, res, next) => {
+  const commentId = req.params.id;
+  const { user } = res.locals.user;
+
+  try {
+    const comment = await Comment.findByPk(commentId);
+    if (!comment) {
+      return res.json({
+        code: 404,
+        message: "댓글을 찾을 수 없습니다",
+      });
+    }
+
+    if (user.id !== comment.commenter) {
+      return res.status(404).json({
+        code: 404,
+        message: "본인이 작성한 댓글만 삭제 가능합니다.",
+      });
+    }
+
+    await comment.destroy();
+
+    return res.status(200).json({
+      code: 200,
+      message: "댓글을 정상적으로 삭제하였습니다.",
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
   }
 };
--- routes/comment.js
+++ routes/comment.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const { verfiyLoginUser } = require("../middlewares");
-const { addComment } = require("../controllers/comment");
+const {
+  addComment,
+  updateComment,
+  deleteComment,
+} = require("../controllers/comment");
 
 const router = express.Router();
 
 router.post("/", verfiyLoginUser, addComment);
+
+router.patch("/:id", verfiyLoginUser, updateComment);
+
+router.delete("/:id", verfiyLoginUser, deleteComment);
 
 module.exports = router;
