f4fbb6f0c49f0378b646247e7d782daa9ef43a6b
minki <alsrl0023@naver.com>
2023-11-20 18:41:15 +0900

    fix: event id로 이벤트 조회하면, 좋아요를 누른 유저, 댓글 모두 전송

--- routes/v2.js
+++ routes/v2.js
@@ -3,8 +3,6 @@ const {
   getEvents,
   increaseViewCount,
   toggleLikeState,
-  getLikedCount,
-  getEventComments,
 } = require("../controllers/v2");
 
 const { verfiyLoginUser } = require("../middlewares");
@@ -18,8 +16,5 @@ router.get("/events/:id", verfiyLoginUser, getEventById, increaseViewCount);
 
 // 좋아요 기능을 위한 라우트 필요
 router.post("/events/:id/likes", verfiyLoginUser, toggleLikeState);
-
-// 댓글 가져오기
-router.get("/events/:id/comments", verfiyLoginUser, getEventComments);
 
 module.exports = router;
